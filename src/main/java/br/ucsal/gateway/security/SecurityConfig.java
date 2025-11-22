0500c0d0089762afb789ac0ee12c98b78d9bfbc5
udimile <udimile@gmail.com>
2025-11-22 18:14:33 -0300

    security

---
+++ src/main/java/br/ucsal/gateway/security/SecurityConfig.java
@@ -0,0 +1,73 @@
+package br.ucsal.gateway.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Value("${api.security.token.secret}")
+    private String secretKey;
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/auth/login").permitAll()
+                        .pathMatchers(HttpMethod.POST, "/auth/register").hasRole("ADMIN")
+                        .pathMatchers("/espaco/**").hasRole("ADMIN")
+                        .pathMatchers("/reservaEspaco/**").hasRole("ADMIN")
+                        .pathMatchers("/professor/**").hasRole("ADMIN")
+                        .pathMatchers("/software/**").hasRole("ADMIN")
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtDecoder(jwtDecoder())
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        )
+                )
+                .build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        var secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
+        return NimbusReactiveJwtDecoder.withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+
+    @Bean
+    public Converter<Jwt, Mono<AbstractAuthenticationToken>> jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("role");
+
+        ReactiveJwtAuthenticationConverter jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
+                new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter)
+        );
+
+        return jwtAuthenticationConverter;
+    }
+}
---
+++ src/main/java/br/ucsal/gateway/security/UserHeaderFilter.java
@@ -0,0 +1,41 @@
+package br.ucsal.gateway.security;
+
+
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class UserHeaderFilter implements GlobalFilter{
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return ReactiveSecurityContextHolder.getContext()
+                .filter(c -> c.getAuthentication() instanceof JwtAuthenticationToken)
+                .flatMap(c -> {
+                    JwtAuthenticationToken auth = (JwtAuthenticationToken) c.getAuthentication();
+
+                    Jwt jwt = (Jwt) auth.getPrincipal();
+
+                    String userId = String.valueOf(jwt.getClaims().get("id"));
+                    String role = String.valueOf(jwt.getClaims().get("role"));
+
+                    ServerWebExchange mutatedExchange = exchange.mutate()
+                            .request(r -> r.headers(headers -> {
+                                headers.add("X-User-Id", userId);
+                                headers.add("X-User-Role", role);
+                            }))
+                            .build();
+
+                    return chain.filter(mutatedExchange);
+                })
+                .switchIfEmpty(chain.filter(exchange));
+    }
+}
